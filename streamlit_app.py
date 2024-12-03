4598c4dfe3fe1211c4404a874f0f1d883b9df132
pvpiv <fmdnyc@gmail.com>
2024-12-03 01:24:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,15 +83,15 @@ with cols[0]:
         popover = st.popover('Settings' ,use_container_width =True)
         if not st.session_state['table_string_butt']:
 
-            show_custom_boxz2 = popover.checkbox('Retro Cup', on_change=upd_cust1, key='sho_cust1')
-            show_custom_boxz = popover.checkbox('Willpower Cup', on_change=upd_cust, key='sho_cust')
+           # show_custom_boxz2 = popover.checkbox('Retro Cup', on_change=upd_cust1, key='sho_cust1')
+           # show_custom_boxz = popover.checkbox('Willpower Cup', on_change=upd_cust, key='sho_cust')
             #show_custom_boxz2 = popover.checkbox('Halloween Cup', on_change=upd_cust1, key='sho_cust1')
             show_shadow_boxz = popover.checkbox('Include Shadow Pokémon', on_change=upd_shadow, key='sho_shad', value=st.session_state['get_shadow'])
 
 
         else:
-            show_custom_boxz2 = popover.checkbox('Retro Cup', on_change=upd_cust1, key='sho_cust1')
-            show_custom_boxz = popover.checkbox('Willpower Cup', on_change=upd_cust, key='sho_cust')
+           # show_custom_boxz2 = popover.checkbox('Retro Cup', on_change=upd_cust1, key='sho_cust1')
+         #   show_custom_boxz = popover.checkbox('Willpower Cup', on_change=upd_cust, key='sho_cust')
             show_gym_box = popover.checkbox('Gym Attackers/Defenders', on_change=update_gym_bool, key='sho_gym')
             popover.divider()
             topstrin = str(st.session_state.top_num)
