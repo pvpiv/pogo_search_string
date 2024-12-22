4d20c160bcb29129b0fdec8cabc6b759458be542
pvpiv <fmdnyc@gmail.com>
2024-12-22 00:38:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -319,14 +319,14 @@ with cols[1]:
                 st.code(make_search_string(df, "master", st.session_state.top_num, fam_box, False, inv_box,show_xl_boxz))
                 lab_mast = "Show Master Table"
                 if st.session_state['master_clicked']:
-                lab_mast  = "Hide Community Day evolve Table"
-                family_data_master = format_data_top(df, 'Master', st.session_state.top_num,True)
-                df_display_master = pd.DataFrame(family_data_master)
-                df_display_master.set_index(['Pokemon'], inplace=True)
-                st.button(lab_mast, on_click = master_but)
-                st.table(df_display_master)
+	                lab_mast  = "Hide Community Day evolve Table"
+	                family_data_master = format_data_top(df, 'Master', st.session_state.top_num,True)
+	                df_display_master = pd.DataFrame(family_data_master)
+	                df_display_master.set_index(['Pokemon'], inplace=True)
+	                st.button(lab_mast, on_click = master_but)
+	                st.table(df_display_master)
                 else:
-                st.button(lab_mast,on_click = master_but)
+                	st.button(lab_mast,on_click = master_but)
 
 		    
             try:
