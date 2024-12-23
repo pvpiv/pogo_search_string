0f3d46e3c138e049f7674a06f22025ae0a9fa2c0
pvpiv <fmdnyc@gmail.com>
2024-12-23 11:33:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -454,6 +454,13 @@ td {
     text-overflow: ellipsis;
     display: block;
 }
+div[data-testid="column"] {
+    width: fit-content !important;
+    flex: unset;
+}
+div[data-testid="column"] * {
+    width: fit-content !important;
+}
 </style>
 """, unsafe_allow_html=True)
 
