ee75da6b51b8e842f87a6685b0f146588661e080
pvpiv <fmdnyc@gmail.com>
2025-02-21 20:15:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ with cols[0]:
            # show_custom_boxz2 = popover.checkbox('Retro Cup', on_change=upd_cust1, key='sho_cust1')
            # show_custom_boxz = popover.checkbox('Mega Master Cup', on_change=upd_cust1, key='sho_cust2')
 
-            show_custom_boxz2 = popover.checkbox('Mega Master Cup', value=st.session_state['show_custom2'], on_change=upd_cust1, key='sho_cust2')
+            show_custom_boxz2 = popover.checkbox('Mega Master Cup', value=st.session_state['show_custom2'], on_change=upd_cust2, key='sho_cust2')
 
             show_shadow_boxz = popover.checkbox('Include Shadow Pokémon', on_change=upd_shadow, key='sho_shad', value=st.session_state['get_shadow'])
 
