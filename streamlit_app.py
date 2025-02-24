13f25a2909a075d69e8192f235ccc49e25401472
pvpiv <fmdnyc@gmail.com>
2025-02-24 01:05:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def configure_ag_grid2(df, cols=None):
                 
                 gb.configure_column( col, maxWidth = 150)                #,"wrapText": True
             else:
-                gb.configure_column( col, maxWidth = int(14*df[col].astype(str).str.len().max()))
+                gb.configure_column( col, maxWidth = int(15*df[col].astype(str).str.len().max()))
      
     custom_css = {".ag-header-cell-text": {"font-size": "16 px",  'font-weight': 700},
     ".ag-theme-streamlit": {'transform': "scale(0.8)", "transform-origin": '0 0'}}
