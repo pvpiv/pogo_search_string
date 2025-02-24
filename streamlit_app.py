79e14e31363f150560011fa8c1853706537380d0
pvpiv <fmdnyc@gmail.com>
2025-02-24 00:51:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def configure_ag_grid2(df, cols=None):
         #    gb.configure_column( col, width=df[col_name].astype(str).str.len().max())
       #  elif col in ('Lvl','Level'):
          #   gb.configure_column( col, width=df[col_name].astype(str).str.len().max())
-    custom_css = {".ag-header-cell-text": {"font-size": "48 px",  'font-weight': 700},
+    custom_css = {".ag-header-cell-text": {"font-size": "24 px",  'font-weight': 700},
     ".ag-theme-streamlit": {'transform': "scale(0.8)", "transform-origin": '0 0'}}
 
     gridOptions = gb.build() 
