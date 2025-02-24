45ea945ffdd7325e20416c0ecf97fb15118e1af3
pvpiv <fmdnyc@gmail.com>
2025-02-23 23:55:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,8 +81,8 @@ def configure_ag_grid2(df, cols=None):
 	
 	grid_table = AgGrid(gridOptions=gridOptions,
 	fit_columns_on_grid_load=True,
-	height=300,
-	width='75%',
+	height=500,
+	width='25%',
 	theme="alpine",
 	reload_data=True,
 	configure_side_bar=True,
