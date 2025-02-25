5a5de9e85ca0d64dda925174f72723e59bdf9252
pvpiv <fmdnyc@gmail.com>
2025-02-24 22:21:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,6 +123,7 @@ with cols[0]:
     )
 
     if st.session_state.show_string: 
+        st.subheader("PVP Poké Search Strings")
         top_nbox = st.number_input('Showing Top:', 
                                     value=st.session_state.top_num, 
                                     key='top_no',
@@ -192,7 +193,7 @@ with cols[1]:
         
         
 
-        st.subheader("PVP Poké Search Strings")
+        
 		
         if st.session_state.show_string:
 
