157282a11e53eda05e63f74e065b75dc8b9fd85c
pvpiv <fmdnyc@gmail.com>
2025-02-24 21:50:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,7 +117,7 @@ with st.container():
                 fam_box = popover.checkbox('Include pre-evolutions', value=True)
                 show_xl_boxz = popover.checkbox('Include XL Pokémon \n\n(XL Candy needed)', on_change=upd_xl, key='sho_xl', value=st.session_state['show_xl'])
                 iv_box = popover.checkbox('Include IV Filter \n\n(Works for Non XL Pokémon)', value=True)
-                inv_box = popover('Invert strings', value=st.session_state.show_inverse, key='show_inv')
+                inv_box = popover.('Invert strings', value=st.session_state.show_inverse, key='show_inv')
                 # tables_pop = st.popover("League Tables")
 
         
