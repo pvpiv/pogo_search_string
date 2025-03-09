fd0d73fe62e85d62f1c8cbd9cf5577ee96ebc500
pvpiv <fmdnyc@gmail.com>
2025-03-09 18:33:25 -0400

    Update session_state_manager.py

--- session_state_manager.py
+++ session_state_manager.py
@@ -8,6 +8,8 @@ def initialize_session_state():
         st.session_state['get_dat'] = False
     if 'get_shadow' not in st.session_state:
         st.session_state['get_shadow'] = True
+    if 'session_data' not in st.session_state:
+        st.session_state['session_data'] = True
     if 'show_xl' not in st.session_state:
         st.session_state['show_xl'] = True
     if 'get_season' not in st.session_state:
