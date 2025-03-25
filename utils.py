8807bb6b162f821e10b8aed0aa1ba0e86f312d1b
pvpiv <fmdnyc@gmail.com>
2025-03-24 23:32:19 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -142,6 +142,10 @@ def get_top_50_ids(df, rank_column, league, top_n, fam, iv_bool, inv_bool, xl_va
 
 
 def make_search_string(df, league, top_n, fam, iv_b, inv_b,sho_xl_val, all_pre=False,shad_only=False):
+    if shad_only:
+        shad_str = '&shadow'
+    else:
+        shad_str = ''
     if league == 'little':
         return get_top_50_ids(df, 'Little_Rank', 'little', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre,shad_only)
     elif league == 'great':
@@ -152,13 +156,13 @@ def make_search_string(df, league, top_n, fam, iv_b, inv_b,sho_xl_val, all_pre=F
         return get_top_50_ids(df, 'Master_Rank', 'master', top_n, fam, iv_b, inv_b,True,all_pre,shad_only)
     elif league == 'all':
         return (
-            get_top_50_ids(df, 'Little_Rank', 'little', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre,shad_only)
+            get_top_50_ids(df, 'Little_Rank', 'little', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre)
             + ','
-            + get_top_50_ids(df, 'Great_Rank', 'great', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre,shad_only)
+            + get_top_50_ids(df, 'Great_Rank', 'great', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre)
             + ','
-            + get_top_50_ids(df, 'Ultra_Rank', 'ultra', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre,shad_only)
+            + get_top_50_ids(df, 'Ultra_Rank', 'ultra', top_n, fam, iv_b, inv_b, sho_xl_val,all_pre)
             + ','
-            + get_top_50_ids(df, 'Master_Rank', 'master', top_n, fam, iv_b, inv_b, True,all_pre,shad_only)
+            + get_top_50_ids(df, 'Master_Rank', 'master', top_n, fam, iv_b, inv_b, True,all_pre)  + shad_str
         )
 
 def format_data_top(df, league, num_rank,xl_var):
