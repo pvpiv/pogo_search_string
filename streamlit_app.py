77d1ed003b792baca7a05304350e5f29c7e00101
pvpiv <fmdnyc@gmail.com>
2025-04-01 22:50:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ season_start = date(2025, 3, 4)
 # Set GitHub API URL based on 'show_custom' flag
 if  st.session_state['show_custom']:
     GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_mpremier.csv'
-    df = pd.read_csv('pvp_data_scroll.csv')
+    df = pd.read_csv('pvp_data_mpremier.csv')
 	
 #elif  st.session_state['show_custom2']:
  # GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_willpower.csv'
