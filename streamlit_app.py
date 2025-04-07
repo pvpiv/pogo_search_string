0ff77e48839e98b1d5efb40ac5872f079caaab37
pvpiv <fmdnyc@gmail.com>
2025-04-07 12:24:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -330,7 +330,7 @@ with cols[1]:
                     st.button(lab_att,on_click = ultra_but)
             except:
                 pass
-        elif st.session_state['show_custom']: 
+        elif st.session_state['show_custom'] and not st.session_state['show_custom2']: 
             try:
                 #popover.button("Show Sunshine Cup Table", key='sun_table', on_click=great_but)
                 days_since_date = calculate_days_since(season_start)
