efabe68c516e708c37e53fa07d8fcbb1c8c1aa57
pvpiv <fmdnyc@gmail.com>
2025-06-16 15:00:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -457,7 +457,7 @@ with cols[1]:
             except:
                 pass
         elif st.session_state['show_custom1']: 
-	    try:
+            try:
                 st.write(f'Ultra League Top {st.session_state.top_num} Search String:')
                 st.code(make_search_string(df, "ultra", st.session_state.top_num, fam_box, iv_box, inv_box, show_xl_boxz, False,shad_only=shad_box, language = st.session_state['language']))
                 lab_ult = "Show Ultra Summer Table"
