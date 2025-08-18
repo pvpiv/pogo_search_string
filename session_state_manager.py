cbffe6391c12c44f8a8d0985da7d7aab2935d387
pvpiv <fmdnyc@gmail.com>
2025-08-18 16:06:51 -0400

    Update session_state_manager.py

--- session_state_manager.py
+++ session_state_manager.py
@@ -33,7 +33,7 @@ def initialize_session_state():
     if 'show_xl' not in st.session_state:
         st.session_state['show_xl'] = True
     if 'get_season' not in st.session_state:
-        st.session_state['get_season'] = True
+        st.session_state['get_season'] = False
     if 'last_sel' not in st.session_state:
         st.session_state['last_sel'] = None
     if 'last_n' not in st.session_state:
