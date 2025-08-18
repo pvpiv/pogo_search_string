748adf66efa341f3bb734b7cb900bdd14c98e1d2
pvpiv <fmdnyc@gmail.com>
2025-08-18 16:09:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ with cols[0]:
                 iv_box = popover.checkbox('Include IV Filter \n\n(Works for Non XL Pokémon)', value=True)
                 inv_box = popover.checkbox('Invert strings', value=st.session_state.show_inverse, key='show_inv')# tables_pop = st.popover("League Tables")
                 shad_box = popover.checkbox('Shadow Only', value=st.session_state.show_shadow, key='sho_shad', on_change=upd_shad_only)
-    
+                season_box = popover.checkbox('Next Season Rankings', value=st.session_state.get_season, key='sho_seas', on_change=upd_seas)
 
         
     st.toggle(
@@ -138,7 +138,7 @@ with cols[0]:
         value = st.session_state['table_string_butt'],
         on_change = upd_tab_str
     )
-    season_box = popover.checkbox('Next Season Rankings', value=st.session_state['get_season'], on_change=upd_seas, key='sho_seas')
+    
 
     with lang_col:
         st.selectbox(
