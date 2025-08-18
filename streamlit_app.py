c9f7a28a147149d9c5bb97b0adfb4221a8401af8
pvpiv <fmdnyc@gmail.com>
2025-08-18 16:24:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ with cols[0]:
         value = st.session_state['table_string_butt'],
         on_change = upd_tab_str
     )
-    
+    show_seas_boxz = st.checkbox('Next Season Rankings', on_change=upd_seas, key='sho_seas', value=st.session_state['get_season'])
 
     with lang_col:
         st.selectbox(
