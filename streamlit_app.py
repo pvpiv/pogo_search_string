2c469168b4cf5768293aa06053573abca27bf461
pvpiv <fmdnyc@gmail.com>
2026-04-14 18:54:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,8 @@ days_since_date = calculate_days_since(season_start)
 age_string = f"age0-{days_since_date}&"
 
 if  st.session_state['show_custom2']:
-	GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_fantasy.csv'
-	df = pd.read_csv('pvp_data_fantasy.csv')
+	GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_spring.csv'
+	df = pd.read_csv('pvp_data_spring.csv')
 #elif  st.session_state['get_season']:
 	#GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_seas.csv'
 	#df = pd.read_csv('pvp_data_seas.csv')
@@ -149,7 +149,7 @@ with cols[0]:
     )
     #show_custom_boxz2 =  
  #   show_seas_boxz = st.checkbox('retro Cup'.title(), value=st.session_state['show_custom2'], on_change=upd_cust2, key='sho_cust2')
-    show_mast_boxz = st.checkbox('Great Fantasy Cup'.title(), value=st.session_state['show_custom2'], on_change=upd_cust2, key='sho_cust2')
+    show_mast_boxz = st.checkbox('Great Spring Cup'.title(), value=st.session_state['show_custom2'], on_change=upd_cust2, key='sho_cust2')
     #show_seas_boxz = st.checkbox('Next Season Rankings', on_change=upd_seas, key='sho_seas', value=st.session_state['get_season'])
 
     with lang_col:
@@ -524,12 +524,12 @@ with cols[1]:
  
 	     
         elif st.session_state['show_custom2']: 
-            lab_gre = "Show Great Fantasy Table".title()
-            st.write(f'Great Fantasy Top {st.session_state.top_num} Search String:')
+            lab_gre = "Show Great Spring Table".title()
+            st.write(f'Great Spring Top {st.session_state.top_num} Search String:')
             st.code(make_search_string(df, "great", st.session_state.top_num, fam_box, iv_box, inv_box, show_xl_boxz, show_only_xl_boxz, False,shad_only=shad_box, language = st.session_state['language']))
             
             if st.session_state['great_clicked']:
-                lab_gre  = "Hide Great Fantasy Table".title()
+                lab_gre  = "Hide Great Spring Table".title()
                 st.button(lab_gre,on_click = great_but)
                 family_data_Great = format_data_top(df, 'Great', st.session_state.top_num,show_xl_boxz,show_only_xl_boxz)
                 df_display_Great = pd.DataFrame(family_data_Great)
@@ -541,7 +541,7 @@ with cols[1]:
 		    
         elif st.session_state['show_custom3']: 
 
-            lab_ult = "Show Ultra Fantasy Cup Table".title()
+            lab_ult = "Show Ultra Spring Cup Table".title()
             st.write(f'Ultra League Top {st.session_state.top_num} Search String:')
             st.code(make_search_string(df, "ultra", st.session_state.top_num, fam_box, iv_box, inv_box, show_xl_boxz, show_only_xl_boxz, False,shad_only=shad_box, language = st.session_state['language']))
             lab_ult = "Show Ultra Table"
@@ -564,7 +564,7 @@ with cols[1]:
             if st.session_state['show_custom']:
                 copy_val = f'*Click string to show Copy button and Paste Top {topstrin} Mega Master Cup into PokeGO {st.session_state['language']}*'
             elif st.session_state['show_custom2']:
-                copy_val = f'*Click string to show Copy button and Paste Top {topstrin} Great fantasy Cup into PokeGO {st.session_state['language']}*'
+                copy_val = f'*Click string to show Copy button and Paste Top {topstrin} Great spring Cup into PokeGO {st.session_state['language']}*'
             elif st.session_state['show_custom3']:
                 copy_val = f'*Click string to show Copy button and Paste Top {topstrin} Great Fossil Cup into PokeGO {st.session_state['language']}*'
             else:
