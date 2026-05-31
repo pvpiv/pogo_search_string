ebe2d5d28ff5a53d56c34ab5beb13d86d45fd74d
pvpiv <fmdnyc@gmail.com>
2026-05-30 21:15:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,9 +70,9 @@ age_string = f"age0-{days_since_date}&"
 if  st.session_state['show_custom3']:
 	GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data.csv'
 	df = pd.read_csv('pvp_data.csv')
-#elif  st.session_state['get_season']:
-	#GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_seas.csv'
-	#df = pd.read_csv('pvp_data_seas.csv')
+elif  st.session_state['get_season']:
+	GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_seas.csv'
+	df = pd.read_csv('pvp_data_seas.csv')
 
 #elif  st.session_state['show_custom3']:
  # GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_Fossil.csv'
@@ -149,8 +149,8 @@ with cols[0]:
     )
     #show_custom_boxz2 =  
  #   show_seas_boxz = st.checkbox('retro Cup'.title(), value=st.session_state['show_custom2'], on_change=upd_cust2, key='sho_cust2')
-    show_mast_boxz = st.checkbox('Great Catch Cup'.title(), value=st.session_state['show_custom3'], on_change=upd_cust3, key='sho_cust3')
-    #show_seas_boxz = st.checkbox('Next Season Rankings', on_change=upd_seas, key='sho_seas', value=st.session_state['get_season'])
+    #show_mast_boxz = st.checkbox('Next Season Preview'.title(), value=st.session_state['show_custom3'], on_change=upd_cust3, key='sho_cust3')
+    show_seas_boxz = st.checkbox('Next Season Rankings', on_change=upd_seas, key='sho_seas', value=st.session_state['get_season'])
 
     with lang_col:
         st.selectbox(
