3734d5c4fbaffb857d9b9dca9e3cfbd377b29fa2
pvpiv <fmdnyc@gmail.com>
2026-06-17 23:29:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,9 @@ if  st.session_state['show_custom2']:
 #	GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_seas.csv'
 #	df = pd.read_csv('pvp_data_seas.csv')
 
-#elif  st.session_state['show_custom3']:
- # GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_Fossil.csv'
- # df = pd.read_csv('pvp_data_Retro.csv')	
+elif  st.session_state['show_custom3']:
+  GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_sunshine.csv'
+  df = pd.read_csv('pvp_data_sunshine.csv')	
 else:
     GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data.csv'
     df = pd.read_csv('pvp_data.csv')
@@ -149,7 +149,8 @@ with cols[0]:
     )
     #show_custom_boxz2 =  
     show_seas_boxz = st.checkbox('Mega Master Cup'.title(), value=st.session_state['show_custom2'], on_change=upd_cust2, key='sho_cust2')
-    #show_mast_boxz = st.checkbox('Next Season Preview'.title(), value=st.session_state['show_custom3'], on_change=upd_cust3, key='sho_cust3')
+	
+    show_mast_boxz = st.checkbox('Great Sunshine Cup'.title(), value=st.session_state['show_custom3'], on_change=upd_cust3, key='sho_cust3')
     #show_seas_boxz = st.checkbox('Next Season Rankings', on_change=upd_seas, key='sho_seas', value=st.session_state['get_season'])
 
     with lang_col:
@@ -492,18 +493,19 @@ with cols[1]:
                 pass
         elif st.session_state['show_custom3']: 
             try:
-                st.write(f'Little League Top {st.session_state.top_num} Search String:')
-                st.code(make_search_string(df, "little", st.session_state.top_num, fam_box, iv_box, inv_box, show_xl_boxz, show_only_xl_boxz, False,shad_only=shad_box, language = st.session_state['language']))
-                lab_lit = "Show Great Fossil Table".title()
-                if st.session_state['little_clicked']:
-                    lab_lit = "Hide Great Fossil Table".title()
-                    st.button(lab_lit,on_click = little_but)
-                    family_data_Little = format_data_top(df, 'Little', st.session_state.top_num,show_xl_boxz,show_only_xl_boxz)
-                    df_display_Little = pd.DataFrame(family_data_Little)
-                    df_display_Little.set_index(['#'])
-                    st.markdown(swap_columns(df_display_Little,"Pokemon","#").style.hide(axis="index").to_html(escape=False), unsafe_allow_html=True)  
-                else: 
-                    st.button(lab_lit,on_click = little_but)   
+				lab_gre = "Show Great Sunshine Cup Table".title()
+				st.write(f'Great Sunshine Top {st.session_state.top_num} Search String:')
+				st.code(make_search_string(df, "great", st.session_state.top_num, fam_box, iv_box, inv_box, show_xl_boxz, show_only_xl_boxz, False,shad_only=shad_box, language = st.session_state['language']))
+				
+				if st.session_state['great_clicked']:
+					lab_gre  = "Hide Great Sunshine Table".title()
+					st.button(lab_gre,on_click = great_but)
+					family_data_Great = format_data_top(df, 'Great', st.session_state.top_num,show_xl_boxz,show_only_xl_boxz)
+					df_display_Great = pd.DataFrame(family_data_Great)
+					df_display_Great.set_index(['#'])
+					st.markdown(swap_columns(df_display_Great,"Pokemon","#").style.hide(axis="index").to_html(escape=False), unsafe_allow_html=True)
+				else:
+					st.button(lab_gre,on_click = great_but)
                 
             except:
                 pass
