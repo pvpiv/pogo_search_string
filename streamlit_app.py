002343f41c3f9f4ee61c30c707ee10f4b1774ea7
pvpiv <fmdnyc@gmail.com>
2026-09-10 12:41:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,8 +79,8 @@ if  st.session_state['show_custom2']:
 #	df = pd.read_csv('pvp_data_seas.csv')
 
 else:
-    GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data_seas.csv'
-    df = pd.read_csv('pvp_data_seas.csv')
+    GITHUB_API_URL = 'https://api.github.com/repos/pvpiv/pogo_search_string/commits?path=pvp_data.csv'
+    df = pd.read_csv('pvp_data.csv')
 
 try:
 	if st.session_state["comm"] == "true":
